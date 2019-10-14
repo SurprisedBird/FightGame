b950d0d4e8cd5bf0c07a375ea1291bb990c04fdc
yavors <irinayavosr@gmail.com>
2019-10-14 14:32:50 +0300

    File constants was changed

--- constants.py
+++ constants.py
@@ -1,8 +1,5 @@
 from warrior import Warrior
 
-API_KEY = "AIzaSyBO4vjZjodHXbmIGjf4B2ZuXGZcYudiips"
-URL = "https://maps.googleapis.com/maps/api/geocode/json"
-
 power_warrior = Warrior("Power man", 15, 1.0, 100)
 healthy_warrior = Warrior("Healthy man", 10, 1.0, 150)
 skill_warrior = Warrior("Skill man", 10, 1.5, 100)
